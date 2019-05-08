8e0b6f5a546149f150053e925d7360cb7e8b35a3
Robert Kempf <rvkempf@gmail.com>
2019-05-08 09:16:52 -0400

    resolved with console log

--- js/utils.js
+++ js/utils.js
@@ -1,18 +1,20 @@
 function doloadGPTScript(resolve, reject) {
-  window.googletag = window.googletag || {};
-  window.googletag.cmd = window.googletag.cmd || [];
+  console.log('RESOLVED IMMEDIATELY JS')
+  resolve(window.googletag)
+  // window.googletag = window.googletag || {};
+  // window.googletag.cmd = window.googletag.cmd || [];
 
-  const scriptTag = document.createElement('script');
-  scriptTag.src = `${document.location.protocol}//www.googletagservices.com/tag/js/gpt.js`;
-  scriptTag.async = true;
-  scriptTag.type = 'text/javascript';
-  scriptTag.onerror = function scriptTagOnError(errs) {
-    reject(errs);
-  };
-  scriptTag.onload = function scriptTagOnLoad() {
-    resolve(window.googletag);
-  };
-  document.getElementsByTagName('head')[0].appendChild(scriptTag);
+  // const scriptTag = document.createElement('script');
+  // scriptTag.src = `${document.location.protocol}//www.googletagservices.com/tag/js/gpt.js`;
+  // scriptTag.async = true;
+  // scriptTag.type = 'text/javascript';
+  // scriptTag.onerror = function scriptTagOnError(errs) {
+  //   reject(errs);
+  // };
+  // scriptTag.onload = function scriptTagOnLoad() {
+  //   resolve(window.googletag);
+  // };
+  // document.getElementsByTagName('head')[0].appendChild(scriptTag);
 }
 
 export function loadGPTScript() {
